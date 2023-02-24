1f6487a985883d8760b03be73d74a5bdb4e0894a
philipp <Philipp Kraft>
2023-02-24 13:17:19 +0100

    bumped version

--- include/version.h
+++ include/version.h
@@ -35,8 +35,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONNR "6.88-JLU2"
-#define DATE "2022/11/24"
+#define VERSIONNR "6.88-JLU3"
+#define DATE "2023/02/24"
 
 
 #define VERSION QString("openLISEM version %1 - %2").arg(VERSIONNR).arg(DATE)
