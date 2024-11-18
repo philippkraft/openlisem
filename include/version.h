b816ed76863c278089fda44b9bcaf698a2305618
vjetten <v.jetten@kpnmail.nl>
2024-11-18 10:10:32 +0100

    // version to 7.4 18 nov 24

--- include/version.h
+++ include/version.h
@@ -35,8 +35,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSIONNR "7.3"
-#define DATE "2024/11/14"
+#define VERSIONNR "7.4"
+#define DATE "2024/11/18"
 
 
 #define VERSION QString("openLISEM version %1 - %2").arg(VERSIONNR).arg(DATE)
